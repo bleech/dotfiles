67f81f89c4adee7ae71f29c17fccdc273c7eb65a
Dominik Tränklein <dominik@bleech.de>
2018-08-23 22:53:51 +0200

    add xcode-select install

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 
 echo "Bootstrapping your dotfiles..."
 
+# Install xcode command line tools
+xcode-select --install
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
